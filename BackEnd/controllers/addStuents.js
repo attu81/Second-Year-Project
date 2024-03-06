c94d64f46bc02c24cf1a043aaafff0efbb581bc4
Anir Jung Thapa <anir234thapa@gmail.com>
2024-03-06 06:18:20 +0545

    adding students

---
+++ BackEnd/controllers/addStuents.js
@@ -0,0 +1,10 @@
+const addStudent = (req,res) =>{
+
+    const {studentName , dob , address, contact, age } = req.body 
+
+    if(!studentName || !dob || !address  || !contact || !age ){
+        
+    }
+
+
+}
