e4ad2da1771f9a2dd0a8ea43bf1728e24a1c2f47
Anir Jung Thapa <anir234thapa@gmail.com>
2024-03-29 06:42:30 +0545

    /

--- BackEnd/controllers/addStudents.js
+++ BackEnd/controllers/addStudents.js
@@ -1,20 +1,24 @@
 const db = require("../config/dbConfig");
 
 const addStudents = (req, res) => {
-  const { studentName, dob, address, contact, doa ,  studentImage} = req.body;
+  const { studentName, dob, address, contact, doa, studentImage } = req.body;
 
-  if (!studentName || !dob || !address || !contact || !doa ) {
-    return res.status(500).json({ message: "Please fill all the details" });
-  }
-
-  const addStudentQuery = "INSERT INTO students VALUES SET ?";
-
-  db.query(addStudentQuery, student, (err, data) => {
-    if (err) {
-      return res.status(400).json({ error: "Error adding student!" });
+  try {
+    if (!studentName || !dob || !address || !contact || !doa) {
+      return res.status(500).json({ message: "Please fill all the details" });
     }
-    return res.status(200).json({ message: "Student added successfully!" });
-  });
+
+    const addStudentQuery = "INSERT INTO students VALUES SET ?";
+
+    db.query(addStudentQuery, student, (err, data) => {
+      if (err) {
+        return res.status(400).json({ error: "Error adding student!" });
+      }
+      return res.status(200).json({ message: "Student added successfully!" });
+    });
+  } catch (error) {
+    res.json({ error: error });
+  }
 };
 
 module.exports = addStudents;
--- BackEnd/controllers/editStudent.js
+++ BackEnd/controllers/editStudent.js
@@ -1,10 +1,7 @@
 const db = require("../config/dbConfig");
 
 const editStudents = async (req, res) => {
+  const student_id = req.body;
 
-    const student_id = req.body;
-    
-
-
+  const editStudent = "";
 };
-
